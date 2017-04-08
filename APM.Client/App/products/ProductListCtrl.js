b389760fa10530e9ccbd4142b6e61fad55c9ba36
Hemant Shelar <hemant.shelar@gmail.com>
2017-04-08 14:20:25 +1000

    Added website

---
+++ APM.Client/App/products/ProductListCtrl.js
@@ -0,0 +1,11 @@
+﻿var module = angular.module("productManagement");
+
+module.controller('ProductListCtrl', function ($scope,productResource) {
+
+    $scope.products = [];
+
+    productResource.query(function (data) {
+        $scope.products = data;
+    });
+
+});
